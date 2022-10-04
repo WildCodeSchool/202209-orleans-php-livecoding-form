4ebe6280f3927880f7430affb6e76b5327ccd3d6
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-10-04 15:54:09 +0200

    style

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>Me contacter</h1>
+    <form action="">
+        <label for="my-firstname">Prénom</label>
+        <input type="text" id="my-firstname" name="firstname">
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
+
+        <label for="message">Message</label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <button>Envoyer</button>
+    </form>
+</body>
+</html>
